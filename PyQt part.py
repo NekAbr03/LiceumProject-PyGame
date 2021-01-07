ea7d87f8c2045854105c82b257a9a8f4977702d8
nekabr <q123dbb-r@ya.ru>
2021-01-07 23:56:56 +0300

    compared design with py file

---
+++ PyQt part.py
@@ -0,0 +1,10 @@
+import sys
+
+from PyQt5.QtWidgets import *
+
+from megagamedesign import Ui_MainWindow
+
+class GameStartWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        
