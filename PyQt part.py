9bd9a27b33311e3a092a72c91d95cfe0da5ca053
nekabr <q123dbb-r@ya.ru>
2021-01-08 01:06:39 +0300

    some changes

--- PyQt part.py
+++ PyQt part.py
@@ -4,7 +4,22 @@ from PyQt5.QtWidgets import *
 
 from megagamedesign import Ui_MainWindow
 
+
 class GameStartWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        
+        self.setGeometry(200, 100, 1000, 650)
+        self.setWindowTitle('Mario')
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = GameStartWidget()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
