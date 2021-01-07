d611643acdf27f80bb83e96940089cb1c1ec96b9
Artem <bedintema@gmail.com>
2021-01-08 01:16:57 +0300

    some commit

--- PyQt part.py
+++ PyQt part.py
@@ -1,10 +1,8 @@
 import sys
-
 from PyQt5.QtWidgets import *
-
 from megagamedesign import Ui_MainWindow
+
 
 class GameStartWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        
