04c822b6e9dd9ade12abc4cebd4aa6ed7afe4558
Artem <bedintema@gmail.com>
2021-01-08 23:11:22 +0300

    design main window 1.0

--- PyQt part.py
+++ PyQt part.py
@@ -1,15 +1,26 @@
 import sys
 
+from PyQt5 import uic
 from PyQt5.QtWidgets import *
-
 from megagamedesign import Ui_MainWindow
 
 
 class GameStartWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi('untitled.ui', self)
         self.setGeometry(200, 100, 1000, 650)
         self.setWindowTitle('Mario')
+        self.pushButton.setObjectName("button_play")
+        self.pushButton_2.setObjectName("button_rez")
+        self.label.setObjectName("label_main")
+        self.pushButton_3.setObjectName("button_exit")
+        self.label_2.setObjectName("label_rez")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label.setObjectName("label")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.label_2.setObjectName("label_2")
 
 
 def except_hook(cls, exception, traceback):
