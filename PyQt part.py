beb1529f232db5a43b7183a6c9b580e59eeeb174
Artem <bedintema@gmail.com>
2021-01-09 01:04:19 +0300

    add func output database, result window(output doesn't work)

--- PyQt part.py
+++ PyQt part.py
@@ -1,31 +1,62 @@
 import sys
-
+import sqlite3
+from PyQt5.QtGui import QFont, QPixmap
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
+
 from megagamedesign import Ui_MainWindow
 
 
-class GameStartWidget(QMainWindow, Ui_MainWindow):
+class GameStartWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('untitled.ui', self)
-        self.setGeometry(200, 100, 1000, 650)
-        self.setWindowTitle('Mario')
-        self.pushButton.setObjectName("button_play")
-        self.pushButton_2.setObjectName("button_rez")
-        self.label.setObjectName("label_main")
-        self.pushButton_3.setObjectName("button_exit")
-        self.label_2.setObjectName("label_rez")
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.label.setObjectName("label")
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.label_2.setObjectName("label_2")
+        self.button_exit.clicked.connect(self.close_window)
+        self.button_rez.clicked.connect(self.open_result)
+
+    def close_window(self):
+        self.close()
+
+    def open_result(self):  # окно с таблицей результатов
+        self.window_res = Results()
+        self.window_res.show()
+
+    def update_res(self):  # обновление результата в label_rez
+        pass
+
+
+class Results(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.font = QFont()
+
+        self.setGeometry(1460, 185, 500, 500)
+        self.setWindowTitle("Результаты")
+
+        self.button_close_res = QPushButton(self)
+        self.button_close_res.resize(150, 50)
+        self.button_close_res.move(350, 430)
+        self.button_close_res.setText('Закрыть')
+
+        self.label_res = QLabel(self)
+        self.label_res.resize(500, 450)
+
+        con = sqlite3.connect('rezults.bd')
+        cur = con.cursor()
+        self.result = cur.execute("""SELECT * FROM rezults
+                                    WHERE rezult > 0""").fetchall()
+        self.out_res()
+
+    def out_res(self):
+
+        res_table = []
+        for i in self.result:
+            res_table.append(str(*i))
+        self.label_res.setText('\n'.join(res_table))
 
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
-
 
 
 if __name__ == '__main__':
